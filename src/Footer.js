f019d26f22150be9a85e3e3d2b737809442919fb
Salmanda <salmanda.aisya@gmail.com>
2022-12-05 08:45:46 +0700

    Edit view code link

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="text-center mt-3">
-      <p><a className="link-light" href="https://github.com/SalmandaAK/fcc-javascript-calculator">View Code in GitHub</a></p>
+      <p><a className="link-light" href="https://github.com/SalmandaAK/fcc-pomodoro-clock">View Code in GitHub</a></p>
       <p>Sound Effect from <a className="link-light" href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=6402">Pixabay</a></p>
     </footer>
   )
